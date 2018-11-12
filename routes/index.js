065f817dab59dc3ef2a032ebd2856f61ff00568e
collins njau <collinsnjau39@gmail.com>
2018-11-12 12:10:08 +0300

    Add endpoint to add and get users

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const UserController = require('../controllers/users/UserController')
+
+module.exports =(app)=>{
+    app.get('/api/v1', (req,res) => res.status(200).send({
+        message:'Welcome to carpool backend'
+    }));
+
+    app.post('/api/v1/user', UserController.make);
+    app.get('/api/v1/user', UserController.show);
+}
